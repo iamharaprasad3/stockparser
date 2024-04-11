e54c19381b7d3cd6e0f0e7a0c4db17708149b5ac
iamharaprasad3 <110249992+iamharaprasad3@users.noreply.github.com>
2024-04-11 23:17:54 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,33 +1,38 @@
 import streamlit as st
 import pandas as pd
-import asyncio
-import httpx
+import requests
 from bs4 import BeautifulSoup
 
-async def scrape(symbol):
-    url = f'https://www.screener.in/company/{symbol}/consolidated/'
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            ratios = soup.find_all('span', class_='name')
-            data = {'ROCE': None, 'ROE': None, 'P/E': None, 'Dividend Yield': None}
-            for ratio in ratios:
-                if 'ROCE' in ratio.text:
-                    roce_value = ratio.find_next('span', class_='number').text
-                    data['ROCE'] = roce_value
-                if 'ROE' in ratio.text:
-                    roe_value = ratio.find_next('span', class_='number').text
-                    data['ROE'] = roe_value
-                if 'P/E' in ratio.text:
-                    pe_value = ratio.find_next('span', class_='number').text
-                    data['P/E'] = pe_value
-                if 'Dividend Yield' in ratio.text:
-                    dy_value = ratio.find_next('span', class_='number').text
-                    data['Dividend Yield'] = dy_value
-            return data
+def scrape(symbol):
+    url = f'https://www.screener.in/company/{symbol}/consolidated/'  # Replace 'example.com' with the actual website domain
+    print(url)
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        ratios = soup.find_all('span', class_='name')
+        data = {'ROCE': None, 'ROE': None, 'P/E': None, 'Dividend Yield': None}  # Default values
+        for ratio in ratios:
+            if 'ROCE' in ratio.text:
+                roce_value = ratio.find_next('span', class_='number').text
+                data['ROCE'] = roce_value
+                # print(roce_value)
+            if 'ROE' in ratio.text:
+                roe_value = ratio.find_next('span', class_='number').text
+                data['ROE'] = roe_value
+                # print(roe_value)
+            if 'P/E' in ratio.text:
+                pe_value = ratio.find_next('span', class_='number').text
+                data['P/E'] = pe_value
+                # print(pe_value)
+            if 'Dividend Yield' in ratio.text:
+                dy_value = ratio.find_next('span', class_='number').text
+                data['Dividend Yield'] = dy_value
+                # print(dy_value)
+        return data
 
-async def main():
+
+def main():
+    # st.title('ROCE Web Scraping App')
     st.sidebar.header('Upload Excel File')
     uploaded_file = st.sidebar.file_uploader('Choose Excel file', type=['xlsx'])
 
@@ -45,22 +50,21 @@ async def main():
             scraped_data = []
             spinner = st.spinner("In Progress...")
             with spinner:
-                tasks = [scrape(symbol.strip()) for symbol in symbols]
-                results = await asyncio.gather(*tasks)
-                for data, buying_price, quantity in zip(results, buying_prices, quantities):
+                for symbol, buying_price, quantity in zip(symbols, buying_prices, quantities):
+                    data = scrape(symbol.strip())
                     if data:
+                        scraped_data.append(data)
                         total_value = buying_price * quantity
-                        data['Total Value'] = total_value
+                        scraped_data[-1]['Total Value'] = total_value
                         
-                        if 'Dividend Yield' in data:
-                            dividend_yield = float(data['Dividend Yield'].replace('%', '')) / 100
+                        if 'Dividend Yield' in scraped_data[-1]:
+                            dividend_yield = float(scraped_data[-1]['Dividend Yield'].replace('%', '')) / 100
                             total_dividend = total_value * dividend_yield
                             dividend_per_share = buying_price * dividend_yield
-                            data['Dividend Per Share'] = dividend_per_share
-                            data['Total Dividend'] = total_dividend
+                            scraped_data[-1]['Dividend Per Share'] = dividend_per_share
+                            scraped_data[-1]['Total Dividend'] = total_dividend
                         else:
-                            data['Total Dividend'] = None
-                    scraped_data.append(data)
+                            scraped_data[-1]['Total Dividend'] = None
             
             scraped_df = pd.DataFrame(scraped_data)
             merged_df = pd.concat([df_uploaded, scraped_df], axis=1)
@@ -83,5 +87,8 @@ async def main():
                 mime='text/csv'
             )
 
+
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
